69a74748a6531d9a098e97b490f2e41f9d14cced
itsmanjeet <itsmanjeet1998@gmail.com>
2025-06-21 10:51:31 +0000

    package/sway: no hard dependency on systemd but on sd-bus provider

--- package/sway/sway.mk
+++ package/sway/sway.mk
@@ -8,13 +8,20 @@ SWAY_VERSION = 1.10
 SWAY_SITE = https://github.com/swaywm/sway/releases/download/$(SWAY_VERSION)
 SWAY_LICENSE = MIT
 SWAY_LICENSE_FILES = LICENSE
-SWAY_DEPENDENCIES = systemd host-pkgconf wlroots json-c pcre cairo pango
+SWAY_DEPENDENCIES = host-pkgconf wlroots json-c pcre cairo pango
 SWAY_CONF_OPTS = \
 	-Dwerror=false \
 	-Dzsh-completions=false \
 	-Dfish-completions=false \
-	-Dman-pages=disabled \
-	-Dsd-bus-provider=libsystemd
+	-Dman-pages=disabled
+
+ifeq ($(BR2_PACKAGE_SYSTEMD),y)
+SWAY_CONF_OPTS += -Dsd-bus-provider=libsystemd
+SWAY_DEPENDENCIES += systemd
+else
+SWAY_CONF_OPTS += -Dsd-bus-provider=basu
+SWAY_DEPENDENCIES += basu
+endif
 
 ifeq ($(BR2_PACKAGE_GDK_PIXBUF),y)
 SWAY_CONF_OPTS += -Dgdk-pixbuf=enabled
